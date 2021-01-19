88f683670d826ca2e7dd69fe421298deaf565ba9
contenfe <2640497297@qq.com>
2021-01-19 13:37:50 +0800

    master

--- src/day_01.cpp
+++ src/day_01.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main(int argc,char ** argv)
 {
-
+    int x=10;
+    cout<<&x<<endl;
+    short sn=100;
+    cout<<&sn<<endl;
+    //定义一个int型指针变量，并初始化
+    int *pInt=&x;//pInt 是int *型.pInt指向的类型：int型
+    cout<<pInt<<endl;
+    //
     cout<<"hello world"<<endl;
     return 0;
 }
